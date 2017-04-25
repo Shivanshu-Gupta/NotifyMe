c557ffeeba96a1ceda69f6d902d54e7f5d05172a
Deepanker Mishra <deepanker27mishra@gmail.com>
2017-04-26 00:43:08 +0530

    shared prefs for notification

--- app/src/main/java/com/softwareengg/project/notifyme/Settings/NotifyMePreferenceFragment.java
+++ app/src/main/java/com/softwareengg/project/notifyme/Settings/NotifyMePreferenceFragment.java
@@ -1,7 +1,12 @@
 package com.softwareengg.project.notifyme.Settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.MultiSelectListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.SwitchPreference;
 
 import com.softwareengg.project.notifyme.R;
 
@@ -11,10 +16,40 @@ import com.softwareengg.project.notifyme.R;
 
 public class NotifyMePreferenceFragment extends PreferenceFragment
 {
+    SharedPreferences sharedPref;
     @Override
     public void onCreate(final Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.notifyme_setting);
+
+        //TODO:set prefs
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        SwitchPreference enableNotification = (SwitchPreference) findPreference("notify");
+        enableNotification.setOnPreferenceChangeListener(new SwitchPreference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putBoolean(getString(R.string.notify), (Boolean) newValue);
+                editor.commit();
+                return true;
+            }
+        });
+
+        MultiSelectListPreference multiSelectListPreference = (MultiSelectListPreference) findPreference("vendors");
+        multiSelectListPreference.setOnPreferenceChangeListener(new MultiSelectListPreference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                //if(!((Boolean) newValue)) {
+                    //TODO: enable notifications  in shared prefs
+
+                //} else {
+                    //TODO: disable notifications in shared prefs
+                //}
+                return true;
+            }
+        });
     }
+
 }
